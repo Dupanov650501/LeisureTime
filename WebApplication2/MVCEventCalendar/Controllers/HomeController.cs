46ea7f4fede446984f1ffb9a5bb468a8fa35e04b
Vladislav Dupanov <dupanovvladislav@gmail.com>
2019-12-02 17:44:16 +0300

    2

---
+++ WebApplication2/MVCEventCalendar/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEventCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Adding()
+        {
+            return View("~/Views/Home/Adding.cshtml");
+        }
+
+        public ViewResult Add(Event element)
+        {   
+            using (MyDatabaseEntities db = new MyDatabaseEntities())
+            {
+                db.Events.Add(element);
+                db.SaveChanges();
+            }
+
+            return View("~/Views/Home/Index.cshtml");
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
